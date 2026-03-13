5917f21984ac2c51d9ee544080174362768e40ed
lephadiSergio <lephadisergiosimphiwe@gmail.com>
2026-03-13 20:40:28 +0200

    Add showModules function to manage display of elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showModules(year){
+
+document.getElementById("y2").style.display="none";
+document.getElementById("y3").style.display="none";
+document.getElementById("y4").style.display="none";
+
+document.getElementById(year).style.display="block";
+
+}
